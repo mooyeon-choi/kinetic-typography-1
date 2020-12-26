8ef5e218cfc9f696c1ea8582215cf7465dd22c70
mooyeon-choi <dus1208@ajou.ac.kr>
2020-12-26 19:05:51 +0900

    Update blurFilter

--- app.js
+++ app.js
@@ -28,11 +28,45 @@ class App {
       resolution: window.devicePixelRatio > 1 ? 2 : 1,
       autoDensity: true,
       powerPreference: "high-performance",
-      backgroundColor: 0xffffff,
+      backgroundColor: 0x2b27f5,
     });
     document.body.appendChild(this.renderer.view);
 
     this.stage = new PIXI.Container();
+
+    const blurFilter = new PIXI.filters.BlurFilter();
+    blurFilter.blur = 10;
+    blurFilter.autoFit = true;
+
+    const fragSource = `
+      precision mediump float;
+      varying vec2 vTextureCoord;
+      uniform sampler2D uSampler;
+      uniform float threshold;
+      uniform float mr;
+      uniform float mg;
+      uniform float mb;
+      void main(void) {
+        vec4 color = texture2D(uSampler, vTextureCoord);
+        vec3 mcolor = vec3(mr, mg, mb);
+        if (color.a > threshold) {
+          gl_FragColor = vec4(mcolor, 1.0);
+        } else {
+          gl_FragColor = vec4(vec3(0.0), 0.0);
+        }
+      }
+    `;
+
+    const uniformsData = {
+      threshold: 0.5,
+      mr: 68.0 / 255.0,
+      mg: 224.0 / 255.0,
+      mb: 255.0 / 255.0,
+    };
+
+    const thresholdFilter = new PIXI.Filter(null, fragSource, uniformsData);
+    this.stage.filters = [blurFilter, thresholdFilter];
+    this.stage.filterArea = this.renderer.screen;
   }
 
   resize() {
